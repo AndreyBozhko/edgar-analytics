43c330881f1ee21913ea8adc3f6593e47abac1bb
Andrey Bozhko <andybozhko@gmail.com>
2018-03-31 05:43:32 -0500

    Update Main.java

--- src/edgarAnalytics/Main.java
+++ src/edgarAnalytics/Main.java
@@ -146,6 +146,7 @@ public class Main {
     public static void main(String[] args) throws Exception
     {
         Main solver = new Main(args[0], args[1], args[2]);
+        solver.performEdgarAnalysis();
     }
 
 }
